b19218424d7dc28fec1d52654f91300dbac8214f
Jconstantz74 <70533059+Jconstantz74@users.noreply.github.com>
2021-04-13 14:42:33 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function item_input_listener(e) {
 
 	qty = parseInt(qty); // if the qty is not blank, ensure it is an integer
 
-	const fabs = row.querySelectorAll(".US, .GM, .JP, .SP, .SK, .TW, .CH"); //capture the shop containers within this row
+	const fabs = row.querySelectorAll(".US, .CH"); //capture the shop containers within this row
 	row.classList.add("active"); // make the row active since we've had input
 
 	// for each of the shop containers
@@ -73,11 +73,6 @@ function calculate_totals() {
 	 * This allows for increases and decreases in quantities to be reflected correctly
 	 */
 	let US = 0;
-	let GM = 0;
-	let JP = 0;
-	let SP = 0;
-	let SK = 0;
-	let TW = 0;
 	let CH = 0;
 
 	/**
@@ -95,7 +90,7 @@ function calculate_totals() {
 		qty = parseInt(qty); //convert quantity to an integer
 
 		//capture all of the shop containers in this row
-		const fabs = row.querySelectorAll(".US, .GM, .JP, .SP, .SK, .TW, .CH");
+		const fabs = row.querySelectorAll(".US, .CH");
 
 		//for each shop
 		fabs.forEach(function (fab) {
@@ -106,30 +101,6 @@ function calculate_totals() {
 
 			if (fab.classList.contains("US")) { //if it's amazon, increase amazon's total
 				US = US + total;
-			}
-
-			if (fab.classList.contains("GM")) { //if it's amazon, increase amazon's total
-				GM = GM + total;
-			}
-
-			if (fab.classList.contains("JP")) { //if it's amazon, increase amazon's total
-				JP = JP + total;
-			}
-
-			if (fab.classList.contains("US")) { //if it's amazon, increase amazon's total
-				US = US + total;
-			}
-
-			if (fab.classList.contains("SP")) { //if it's amazon, increase amazon's total
-				SP = SP + total;
-			}
-
-			if (fab.classList.contains("SK")) { //if it's amazon, increase amazon's total
-				SK = SK + total;
-			}
-
-			if (fab.classList.contains("TW")) { //if it's amazon, increase amazon's total
-				TW = TW + total;
 			}
 
 			if (fab.classList.contains("CH")) { //if it's amazon, increase amazon's total
@@ -146,42 +117,17 @@ function calculate_totals() {
 	total_row.classList.add("active"); // make the total row visible since there are numbers to display
 
 	total_row.querySelector('.US span').innerHTML = round_number(US); // set the total row's amazon container content to be the round number for amazon's total
-	total_row.querySelector('.GM span').innerHTML = round_number(GM); // set the total row's peapod container content to be the round number for peapod's total
-	total_row.querySelector('.JP span').innerHTML = round_number(JP);
-	total_row.querySelector('.SP span').innerHTML = round_number(SP);
-	total_row.querySelector('.SK span').innerHTML = round_number(SK);
-	total_row.querySelector('.TW span').innerHTML = round_number(TW);
 	total_row.querySelector('.CH span').innerHTML = round_number(CH);
 	// set the total row's freshdirect container content to be the round number for freshdirect's total
 
 	/**
 	 * Now let's use conditionals to determine which shop cost least
 	 */
-	if (US < GM && US < JP && US < SP && US < SK && US < TW && US < CH) { // if amazon's cost is less than both peapod and freshdirect, amazon is cheapest
+	if (US < CH) { // if amazon's cost is less than both peapod and freshdirect, amazon is cheapest
 		cheapest = 'U.S.';
 	}
 
-	if (GM < US && GM < JP && GM < SP && GM < SK && GM < TW && GM < CH) { // if peapod's cost is less than both amazon and freshdirect, peapod is cheapest
-		cheapest = 'Germany';
-	}
-
-	if (JP < US && JP < GM && JP < SP && JP < SK && JP < TW && JP < CH) { // if freshdirect's cost is less than both peapod and amazon, freshdirect is cheapest
-		cheapest = 'Japan';
-	}
-
-	if (SP < GM && SP < JP && SP < US && SP < SK && SP < TW && SP < CH) { // if freshdirect's cost is less than both peapod and amazon, freshdirect is cheapest
-		cheapest = 'Singapore';
-	}
-
-	if (SK < US && SK < JP && SK < SP && SK < GM && SK < TW && SK < CH) { // if freshdirect's cost is less than both peapod and amazon, freshdirect is cheapest
-		cheapest = 'South Korea';
-	}
-
-	if (TW < GM && TW < JP && TW < SP && TW < SK && TW < US && TW < CH) { // if freshdirect's cost is less than both peapod and amazon, freshdirect is cheapest
-		cheapest = 'Taiwan';
-	}
-
-	if (CH < GM && CH < JP && CH < SP && CH < SK && CH < TW && CH < US) { // if freshdirect's cost is less than both peapod and amazon, freshdirect is cheapest
+	if (CH < US) { // if freshdirect's cost is less than both peapod and amazon, freshdirect is cheapest
 		cheapest = 'China';
 	}
 
